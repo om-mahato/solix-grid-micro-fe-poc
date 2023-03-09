1456312005261341c28fc11767a0545a01e48894
om mahato <ommahato121@gmail.com>
2023-03-09 15:26:26 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// render micro frontend function
+window.rendercreatereactapp = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history}/>,
+    document.getElementById(containerId)
+  );
+  serviceWorker.unregister();
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// unmount micro frontend function
+window.unmountcreatereactapp = containerId => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+// Mount to root if it is not a micro frontend
+if (!document.getElementById('createreactgridapp-container')) {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
